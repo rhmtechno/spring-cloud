f7ef0b7d5293a4e9bb32477ad4bc1d1ae640165b
rakib <rfinfo247@gmail.com>
2021-03-08 17:42:14 +0600

    hystrix and hystrix dashboard added

--- hystrix-dashboard/src/main/java/com/rhmtech/hystrix/dashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/rhmtech/hystrix/dashboard/HystrixDashboardApplication.java
@@ -1,17 +1,16 @@
-package com.rhmtech.cloud.gateway;
+package com.rhmtech.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableHystrix
-public class CloudGatewayApplication {
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
+		SpringApplication.run(HystrixDashboardApplication.class, args);
 	}
 
 }
